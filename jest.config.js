5ef04f5998fbeaedf6c099672d6fb119605ca250
Ying Chen <ashleycheny@gmail.com>
2022-06-30 11:09:53 -0700

    note(jest): install and configure jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // Files jest will parse and transform
+    moduleFileExtensions: ['jest', 'vue', 'js'],
+    // Don't transform files
+    transformIgnorePatterns: ['<rootDirc>/node_modules']
+}
---
+++ src/services/chatMessageService/chatMessageService.spec.js
@@ -0,0 +1,3 @@
+describe("Chat Message Service", () => {
+    it.todo("should return an array of messages");
+});
